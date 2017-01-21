d780588f952c5b02eda2cb18091347dbfce76ef4
DELL <DELL@DELL-PC>
2017-01-21 13:42:49 +0530

    created food ordering system

--- foodorderingsystem/2.pr_set_stock.sql
+++ foodorderingsystem/2.pr_set_stock.sql
@@ -1,10 +1,10 @@
-/* Procedure to set the stocks for the day*/
+/* Procedure to set the quantity for the foods */
 DELIMITER$$
 CREATE PROCEDURE set_stock()
 BEGIN
 DECLARE id_param INT DEFAULT 1;
 REPEAT
-UPDATE food_stocks SET remaining=(SELECT quantity FROM foodtype WHERE id=id_param) WHERE mealid=id_param ;
+UPDATE food_stocks SET quantity=(SELECT quantity FROM foodtype WHERE id=id_param) WHERE mealid=id_param ;
 SET id_param= id_param+1;
 UNTIL (id_param>4) 
  END REPEAT; 
