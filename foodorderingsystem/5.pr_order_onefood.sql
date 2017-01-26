d23ee39fd36f14c09fa24cebb83ee1ccff0f635f
DELL <DELL@DELL-PC>
2017-01-26 11:07:24 +0530

    created food ordering system

--- foodorderingsystem/5.pr_order_onefood.sql
+++ foodorderingsystem/5.pr_order_onefood.sql
@@ -1,46 +1,57 @@
-/*Procedure for ordering a single food*/
-DELIMITER $$
+/* Procedure to check quantity constraints */
+ DROP PROCEDURE pr_update_remainingstock
+ DELIMITER $$
+CREATE PROCEDURE pr_update_remainingstock(IN i_seatparam INT, IN i_f_id INT, IN i_qty INT)
+BEGIN 
+DECLARE sum_qty INT;         
+DECLARE qty INT;
+DECLARE remaining_qty INT;
+ IF i_f_id IN(SELECT food_id FROM `food_transaction` WHERE DATE(date_of_order)=CURDATE()) THEN 
+    SELECT quantity INTO qty FROM food_stocks WHERE foodid=i_f_id; 
+    SELECT SUM(quantity) INTO sum_qty FROM food_transaction WHERE food_id=i_f_id AND order_status='ordered';
+    SET remaining_qty=qty-sum_qty;
+     ELSE
+ SELECT quantity INTO qty FROM food_stocks WHERE foodid=i_f_id; 
+ SET remaining_qty=qty;
+ END IF;
+  IF remaining_qty <> 0 THEN
+        IF remaining_qty>=i_qty THEN
+ START TRANSACTION;
+        SET autocommit=0;
+         INSERT INTO food_transaction(seat_no,food_id,quantity) VALUES(i_seatparam,i_f_id,i_qty);
+        COMMIT;
+        SELECT  'Food is ordered.';
+        ELSE   
+         SELECT 'Quantity ordered is greater than remaining quantity';
+        END IF;
+ELSE 
+  SELECT 'Sorry, this particular food is over';
+END IF; 
+ END $$    
+DELIMITER ;
 
-USE `rashika_db`$$
 
-DROP PROCEDURE IF EXISTS `one_food`$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `pr_order_one_food`(IN seat_number INT,IN i_food_name VARCHAR(100),IN qty INT)
-BEGIN
+/* Procedure to order a single food*/ 
+ DELIMITER$$
+ CREATE PROCEDURE pr_order_onefood(IN seat_number INT,IN i_food_name VARCHAR(100),IN qty INT)
+ BEGIN
  DECLARE foodparam INT;
  DECLARE no_service INT;
  DECLARE f_id INT;
  DECLARE check_food INT;
- DECLARE counts INT;
- DECLARE amount_req INT;
- DECLARE rem INT;
  SELECT fn_check_service() INTO no_service;
  SELECT is_food_valid(i_food_name) INTO check_food;
 IF no_service=1 THEN 
  IF check_food=1 THEN
 SELECT food_stocks.foodid INTO f_id FROM food JOIN food_stocks ON food_stocks.foodid=food.id JOIN foodtype ON food_stocks.mealid=foodtype.id
     WHERE food.food_name=i_food_name AND CURTIME() BETWEEN foodtype.from_time AND  foodtype.to_time;
-  IF EXISTS(SELECT id FROM seats WHERE id=seat_number) THEN
-  IF EXISTS(SELECT foodid FROM food_stocks WHERE foodid=f_id) THEN
+ 
+    IF EXISTS(SELECT foodid FROM food_stocks WHERE foodid=f_id) THEN
    
    SELECT mealid INTO foodparam FROM food_stocks WHERE foodid=f_id;
     IF (foodparam IN(SELECT id FROM foodtype WHERE  from_time<=CURTIME() AND to_time>=CURTIME())) THEN
-     IF (f_id IN(SELECT food_id FROM `food_transaction` WHERE DATE(date_of_order)=CURDATE()))
- THEN
-	SELECT SUM(quantity) INTO counts FROM `food_transaction` WHERE food_id=f_id AND order_status='ORDERED';
-	SELECT quantity INTO amount_req FROM `food_stocks` WHERE mealid=foodparam AND foodid=f_id;
-	SET rem = amount_req - counts;
- 
- ELSE
-	SELECT quantity INTO amount_req FROM food_stocks WHERE mealid=foodparam AND foodid=f_id;
-	SET rem=amount_req;
- END IF;
- IF (qty>rem) THEN
-   SELECT 'ordered quantity is greater than remaining quantity';
-   ELSE
-       INSERT INTO food_transaction(seat_no,food_id,quantity) VALUES(seat_number,f_id,qty);
-       SELECT 'Food ordered';
- END IF;
+   CALL pr_update_remainingstock(seat_number,f_id,qty);
    ELSE
     SELECT 'Sorry food cannot be ordered due to time constraints' AS unordered;
     END IF;
@@ -48,15 +59,10 @@ SELECT food_stocks.foodid INTO f_id FROM food JOIN food_stocks ON food_stocks.fo
    SELECT 'food not available at the moment' AS aplogies;
   END IF;
  ELSE
-  SELECT 'Invalid seat number' AS invalid;  
- END IF;
- ELSE
  SELECT 'Food not in the list' AS invalid_food;
  END IF;
  ELSE
   SELECT 'Sorry.No service provided at this time' AS apologies;
   END IF;
- END$$
-
-DELIMITER ;
-
+ END $$
+ DELIMITER ; 
--- foodorderingsystem/6.pr_place_orders.sql
+++ foodorderingsystem/6.pr_place_orders.sql
@@ -1,38 +1,65 @@
+
+/*Function to check seat available or not*/
+  DELIMITER$$
+ CREATE FUNCTION fn_is_seat_available(i_id INT)
+ RETURNS BOOLEAN
+ BEGIN
+ DECLARE flag BOOLEAN;
+ IF EXISTS(SELECT id FROM seats WHERE  id=i_id AND seat_status='available') THEN
+    IF EXISTS(SELECT id FROM seats WHERE id=i_id AND availability=0) THEN
+     UPDATE seats SET availability=1 WHERE id=i_id;
+     SET flag=TRUE;
+     ELSE 
+     SET flag=FALSE;
+    END IF;
+ END IF;
+ RETURN flag;
+  END $$
+  DELIMITER ;
+
 /*procedure to order 5 food items*/
 DELIMITER$$
-  CREATE PROCEDURE place_orders(IN seatno INT,IN list_of_items MEDIUMTEXT,IN quantity_of_items MEDIUMTEXT)
+  CREATE PROCEDURE pr_place_orders(IN i_seat INT,IN list_of_items MEDIUMTEXT,IN quantity_of_items MEDIUMTEXT)
     BEGIN
-          DECLARE _next TEXT DEFAULT NULL ;
-          DECLARE _q TEXT DEFAULT NULL;
-          DECLARE _qnext INT DEFAULT NULL;
+          DECLARE next_item TEXT DEFAULT NULL ;
+          DECLARE item_qty TEXT DEFAULT NULL;
+          DECLARE next_qty INT DEFAULT NULL;
           DECLARE _nextlen INT DEFAULT NULL;
           DECLARE _value TEXT DEFAULT NULL;
           DECLARE _val TEXT DEFAULT NULL;
+           DECLARE seat_no BOOLEAN;
+
             DECLARE temp INT;
                 SET temp = (LENGTH(list_of_items)-LENGTH(REPLACE(list_of_items,',',''))) + 1;
           IF (temp<=5) THEN 
+       SELECT fn_is_seat_available(i_seat) INTO seat_no;
+   IF seat_no=TRUE THEN
+ UPDATE seats SET seat_status='unavailable'  WHERE id=i_seat;
          iterator :
          LOOP   
             IF LENGTH(TRIM(list_of_items)) = 0 OR list_of_items IS NULL THEN
               LEAVE iterator;
               END IF;
-                SET _next = SUBSTRING_INDEX(list_of_items,',',1); 
-                SET _q=SUBSTRING_INDEX(quantity_of_items,',',1);
-                 SET _nextlen = LENGTH(_next);
-                 SET _qnext = LENGTH(_q);
-                 SET _value = TRIM(_next);
-                 SET _val= TRIM(_q);
+                SET next_item = SUBSTRING_INDEX(list_of_items,',',1); 
+                SET item_qty=SUBSTRING_INDEX(quantity_of_items,',',1);
+                 SET _nextlen = LENGTH(next_item);
+                 SET next_qty = LENGTH(item_qty);
+                 SET _value = TRIM(next_item);
+                 SET _val= TRIM(item_qty);
 
                SET list_of_items = INSERT(list_of_items,1,_nextlen + 1,'');
-               SET quantity_of_items = INSERT(quantity_of_items,1,_qnext + 1,'');
-                CALL `pr_order_onefood`(seatno,_next,_q);
-             END LOOP;
+               SET quantity_of_items = INSERT(quantity_of_items,1,next_qty + 1,'');
+                CALL `pr_order_onefood`(i_seat,next_item,item_qty);
    SELECT food_transaction.order_no,food_transaction.seat_no,food.food_name,food_transaction.quantity 
    FROM food_transaction JOIN food 
-   ON food_transaction.food_id=food.id WHERE food_transaction.seat_no=seatno AND food_transaction.date_of_order=NOW() ;
+   ON food_transaction.food_id=food.id WHERE food_transaction.seat_no=i_seat AND food_transaction.date_of_order=NOW() ;
+         END LOOP;
+                 UPDATE seats SET  seat_status='available',availability=0 WHERE id=i_seat;
 
-       ELSE
+      END IF;
+      ELSE
         SELECT 'You can order only 5 items' AS comments;
-        END IF; 
+        END IF;
+        
     END $$
   DELIMITER ;
